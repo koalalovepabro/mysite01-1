bd15950a406877326b8d864daf3826c9dfa3b065
bitacademy-python <bitacademy.python@gmail.com>
2021-04-07 15:18:24 +0900

    board app 만들기 #3

--- board/views.py
+++ board/views.py
@@ -1,8 +1,31 @@
+from math import ceil
+
 from django.shortcuts import render
+
+from board import models
+
+LIST_COUNT = 10
 
 
 def index(request):
-    return render(request, 'board/index.html')
+    page = request.GET["p"]
+
+    totalcount = models.count()
+    boardlist = models.findall(page, LIST_COUNT)
+
+    # paging 정보를 계산
+    pagecount = ceil(totalcount / LIST_COUNT)
+
+    data = {
+        "boardlist": boardlist,
+        'pagecount': pagecount,
+        'netpage': 7,
+        'prvpage': 5,
+        'curpage': 2
+
+    }
+
+    return render(request, 'board/index.html', data)
 
 
 def view(request):
